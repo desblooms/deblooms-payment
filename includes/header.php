d5f713b47d9b15908289ffe6ac3b9d85f8958a66
Desblooms <desblooms@gmail.com>
2025-05-15 00:54:40 +0530

    Folder Commit

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@
             background: rgba(30, 30, 30, 0.5);
             backdrop-filter: blur(8px);
             -webkit-backdrop-filter: blur(8px);
-            border: 1px solid rgba(255, 255, 255, 0.1);
+            border: 1px solid rgba(255, 255, 255, 0.07);
         }
 
         /* Custom Scrollbar */
